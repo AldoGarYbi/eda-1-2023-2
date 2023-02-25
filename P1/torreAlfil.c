fd146a59beb354b5f74abdbb7bfa59f3332a1226
Aldo Garcia <ybigar05@gmail.com>
2023-02-25 22:24:49 +0000

    primer-avance

---
+++ P1/torreAlfil.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+
+void posicion (int eleccion);
+
+int main (){
+
+    while (1){
+
+        int elec;
+
+        printf("\nMOVIMIENTOS VALIDOS EN PIEZAS DE AJEDREZ\n");
+        printf("\n1- Alfil. ");
+        printf("\n2- Torre. ");
+        printf("\n3- Salir. \n");
+        scanf("%i", &elec);
+
+        switch(elec){
+
+            case 1:
+                posicion(elec);
+            break;
+
+            case 2:
+                posicion(elec);
+            break;
+
+            case 3:
+
+                return 0;
+
+            break;    
+
+        }
+
+
+    }
+
+    return 0;
+
+}
+
+void posicion(int eleccion){
+
+    int x, y;
+
+    printf("\nSeleccione la posicion de la pieza: ");
+    printf("\nPara x: ");
+    scanf("%i", &x);
+    printf("\nPara y: ");
+    scanf("%i", &y);
+
+    int tablero[8][8];
+
+    for(int i = 0; i < 8; i++){
+
+        for(int j = 0; j < 8; j++){
+
+            tablero[i][j] = 0;
+
+        }
+
+    }
+
+    if(eleccion == 1){
+        
+        int cont = 0;
+
+        for(int i = 0; i < 8; i++){
+
+            for(int j = 0; j < 8; j++){
+
+                if(i == x-1){
+
+                    tablero[i+cont][j+y-1] = 2;
+                    tablero[i-cont][j+y-1] = 2;
+                    cont++;
+
+                }
+
+            }
+
+        }
+
+    }
+
+    if(eleccion == 2){
+
+        for(int i = 0; i < 8; i++){
+
+            for(int j = 0; j < 8; j++){
+
+                if(i == x-1){
+
+                    tablero[i][j] = 2;
+
+                }
+
+                if(j == y-1){
+
+                    tablero[i][j] = 2;
+
+                }
+
+            }
+
+        }
+
+    }
+
+    tablero [x-1][y-1] = 1;
+
+    for(int i = 0; i < 8; i++){
+
+        for(int j = 0; j < 8; j++){
+
+            if(tablero [i][j] == 0){
+
+                printf("[ ] ");
+
+            } else if(tablero [i][j] == 1){
+
+                if(eleccion == 1){
+
+                    printf("[A] ");
+
+                }else if(eleccion == 2){
+
+                    printf("[T] ");
+
+                }
+
+            }else if(tablero [i][j] == 2){
+
+                printf("[X] ");
+
+            }
+
+        }
+
+        printf("\n");
+
+    }
+
+    for(int i = 0; i < 8; i++){
+
+        for(int j = 0; j < 8; j++){
+
+            printf("%i ", tablero[i][j]);
+
+        }
+
+        printf("\n");
+
+    }
+
+}
