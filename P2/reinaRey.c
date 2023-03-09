4dc6b3c360fd536e1821be41a4aa3cba076cd93c
Aldo Garcia <ybigar05@gmail.com>
2023-03-09 21:10:31 +0000

    FinalReinaRey2

--- P2/reinaRey.c
+++ P2/reinaRey.c
@@ -46,219 +46,232 @@ int main (){
 
 void posicion(int seleccion){
 
-    int tablero[8][8], i, j, x, y;
+    int tablero[8][8], i, j, x, y, ciclo = 1;
     int *apptab;
 
-    apptab = &tablero[0][0];
+    while(ciclo == 1){
 
-    printf("\nElija la posicion de la pieza en el tablero: ");
-    printf("\nPara x: ");
-    scanf("%d", &x);
-    printf("\nPara y: ");
-    scanf("%d", &y);
-    printf("\n");
+        apptab = &tablero[0][0];
 
-    int auxx = x, auxy = y;
+        printf("\nElija la posicion de la pieza en el tablero entre 1 y 8: ");
+        printf("\nPara x: ");
+        scanf("%d", &x);
+        printf("\nPara y: ");
+        scanf("%d", &y);
+        printf("\n");
 
-    for(i = 0; i < 64; i++){
+        if(x >= 1 && y >= 1 && x <= 8 && y <= 8){
 
-        *apptab = 0;
-        apptab++;
+            ciclo = 0;
 
-    }
+            int auxx = x, auxy = y;
 
-    apptab = &tablero[0][0];
+            for(i = 0; i < 64; i++){
 
-     if(seleccion == 1){
-            
-            for(i = 0; i < 8; i++){
-
-                *(apptab+((y-1)*8)) = 2;
+                *apptab = 0;
                 apptab++;
 
             }
 
             apptab = &tablero[0][0];
 
-            for(i = 0; i < 8; i++){
-                
-                *(apptab+(x-1)) = 2;
-                apptab += 8;
-
-            }
-
-            apptab = &tablero[0][0];
-
-            while((auxy-1) < 8 && (auxx-1) < 8){
-
-                *(apptab+(((y-1)*8)+x-1)) = 2;
-                apptab += 9; 
-                auxy++;
-                auxx++;
-
-            }
-
-            auxx = x; 
-            auxy = y;
-
-            apptab = &tablero[0][0];
-
-            while((auxy+8) > 8 && (auxx-1) <= 8){
-
-                *(apptab+(((y-1)*8)+x-1)) = 2;
-                apptab -= 7;
-                auxy--;
-                auxx++;
-
-            }
-
-            auxx = x; 
-            auxy = y;
-
-            apptab = &tablero[0][0];
-
-            while((auxy-1) < 8 && (auxx+8) > 8){
-
-                *(apptab+(((y-1)*8)+x-1)) = 2;
-                apptab += 7;
-                auxy++;
-                auxx--;
-
-            }
-
-            auxx = x; 
-            auxy = y;
-
-            apptab = &tablero[0][0];
-
-            while((auxy+8) > 8 && (auxx+8) > 8){
-
-                *(apptab+(((y-1)*8)+x-1)) = 2;
-                apptab -= 9;
-                auxy--;
-                auxx--;
-
-            }
-
-            auxx = x; 
-            auxy = y;
-
-            apptab = &tablero[0][0];
-
-    }
-
-    if(seleccion == 2){
-
-        if(y-1 < 7){
-
-            apptab += 8;
-            *(apptab+(((y-1)*8)+x-1)) = 2;
-        
-        }
-
-        apptab = &tablero[0][0];
-
-        if(y-1 > 0){
-
-            apptab -= 8;
-            *(apptab+(((y-1)*8)+x-1)) = 2;
-
-        }
-
-        apptab = &tablero[0][0];
-
-        if(x-1 < 7){
-
-            apptab++;
-            *(apptab+(((y-1)*8)+x-1)) = 2;
-
-        }
-
-        apptab = &tablero[0][0];
-
-        if(x-1 > 0){
-            
-            apptab--;
-            *(apptab+(((y-1)*8)+x-1)) = 2;
-
-        }
-
-        apptab = &tablero[0][0];
-
-        if(y-1 < 7 && x-1 < 7){
-
-            apptab+=9;
-            *(apptab+(((y-1)*8)+x-1)) = 2;
-
-        }
-
-        apptab = &tablero[0][0];
-
-        if(y-1 > 0 && x-1 > 0){
-
-            apptab-=9;
-            *(apptab+(((y-1)*8)+x-1)) = 2;
-
-        }
-
-        apptab = &tablero[0][0];
-
-        if(y-1 < 7 && x-1 > 0){
-
-            apptab+=7;
-            *(apptab+(((y-1)*8)+x-1)) = 2;
-
-        }
-
-        apptab = &tablero[0][0];
-
-        if(y-1 > 0 && x-1 < 7){
-
-            apptab-=7;
-            *(apptab+(((y-1)*8)+x-1)) = 2;
-
-        }
-
-        apptab = &tablero[0][0];
-
-    }
-
-    *(apptab+(((y-1)*8)+x-1)) = 1;
-
-    for(i = 0; i < 64; i++){
-
-       if(*(apptab+i) == 0){
-
-            printf("[ ]");
-
-       }else if(*(apptab+i) == 1){
-
             if(seleccion == 1){
+                    
+                    for(i = 0; i < 8; i++){
 
-                printf("[Q]");
+                        *(apptab+((y-1)*8)) = 2;
+                        apptab++;
+
+                    }
+
+                    apptab = &tablero[0][0];
+
+                    for(i = 0; i < 8; i++){
+                        
+                        *(apptab+(x-1)) = 2;
+                        apptab += 8;
+
+                    }
+
+                    apptab = &tablero[0][0];
+
+                    while((auxy-1) < 8 && (auxx-1) < 8){
+
+                        *(apptab+(((y-1)*8)+x-1)) = 2;
+                        apptab += 9; 
+                        auxy++;
+                        auxx++;
+
+                    }
+
+                    auxx = x; 
+                    auxy = y;
+
+                    apptab = &tablero[0][0];
+
+                    while((auxy+8) > 8 && (auxx-1) < 8){
+
+                        *(apptab+(((y-1)*8)+x-1)) = 2;
+                        apptab -= 7;
+                        auxy--;
+                        auxx++;
+
+                    }
+
+                    auxx = x; 
+                    auxy = y;
+
+                    apptab = &tablero[0][0];
+
+                    while((auxy-1) < 8 && (auxx+8) > 8){
+
+                        *(apptab+(((y-1)*8)+x-1)) = 2;
+                        apptab += 7;
+                        auxy++;
+                        auxx--;
+
+                    }
+
+                    auxx = x; 
+                    auxy = y;
+
+                    apptab = &tablero[0][0];
+
+                    while((auxy+8) > 8 && (auxx+8) > 8){
+
+                        *(apptab+(((y-1)*8)+x-1)) = 2;
+                        apptab -= 9;
+                        auxy--;
+                        auxx--;
+
+                    }
+
+                    auxx = x; 
+                    auxy = y;
+
+                    apptab = &tablero[0][0];
 
             }
 
             if(seleccion == 2){
 
-                printf("[R]");
+                if(y-1 < 7){
+
+                    apptab += 8;
+                    *(apptab+(((y-1)*8)+x-1)) = 2;
+                
+                }
+
+                apptab = &tablero[0][0];
+
+                if(y-1 > 0){
+
+                    apptab -= 8;
+                    *(apptab+(((y-1)*8)+x-1)) = 2;
+
+                }
+
+                apptab = &tablero[0][0];
+
+                if(x-1 < 7){
+
+                    apptab++;
+                    *(apptab+(((y-1)*8)+x-1)) = 2;
+
+                }
+
+                apptab = &tablero[0][0];
+
+                if(x-1 > 0){
+                    
+                    apptab--;
+                    *(apptab+(((y-1)*8)+x-1)) = 2;
+
+                }
+
+                apptab = &tablero[0][0];
+
+                if(y-1 < 7 && x-1 < 7){
+
+                    apptab+=9;
+                    *(apptab+(((y-1)*8)+x-1)) = 2;
+
+                }
+
+                apptab = &tablero[0][0];
+
+                if(y-1 > 0 && x-1 > 0){
+
+                    apptab-=9;
+                    *(apptab+(((y-1)*8)+x-1)) = 2;
+
+                }
+
+                apptab = &tablero[0][0];
+
+                if(y-1 < 7 && x-1 > 0){
+
+                    apptab+=7;
+                    *(apptab+(((y-1)*8)+x-1)) = 2;
+
+                }
+
+                apptab = &tablero[0][0];
+
+                if(y-1 > 0 && x-1 < 7){
+
+                    apptab-=7;
+                    *(apptab+(((y-1)*8)+x-1)) = 2;
+
+                }
+
+                apptab = &tablero[0][0];
 
             }
 
-       }else if(*(apptab+i) == 2){
+            *(apptab+(((y-1)*8)+x-1)) = 1;
 
-            printf("[X]");
+            for(i = 0; i < 64; i++){
 
-       }
+                if(*(apptab+i) == 0){
+
+                        printf("[ ]");
+
+                }else if(*(apptab+i) == 1){
+
+                        if(seleccion == 1){
+
+                            printf("[Q]");
+
+                        }
+
+                        if(seleccion == 2){
+
+                            printf("[R]");
+
+                        }
+
+                }else if(*(apptab+i) == 2){
+
+                        printf("[X]");
+
+                }
 
 
 
-       if((i+1)%8 == 0){
+                if((i+1)%8 == 0){
 
-            printf("\n");
+                        printf("\n");
 
-       }
+                }
 
-    }
+            }
+        }else{
+            
+            printf("\nHa dado valores invalidos, por favor de valores entre 1 y 8.\n");
+            ciclo = 1;
+
+        }
+    }    
 
 }
