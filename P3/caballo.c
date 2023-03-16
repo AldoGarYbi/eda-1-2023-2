47c0dbabf1e12c8ff7865a81b4df007ba462c888
Aldo Garcia <ybigar05@gmail.com>
2023-03-16 18:23:22 +0000

    FinalCaballo1

--- P3/caballo.c
+++ P3/caballo.c
@@ -33,6 +33,10 @@ struct caballo{
 void inicializar(int tablero[8][8]);
 void imprimir(int tablero[8][8], int selec);
 void caballo();
+void alfil();
+void torre();
+void reina();
+void rey();
 
 int main(){
 
@@ -59,15 +63,27 @@ int main(){
             break;
 
             case 2:
+
+                alfil();
+
             break;
 
             case 3:
+
+                torre();
+
             break;
 
             case 4:
+
+                reina();
+
             break;
 
             case 5:
+
+                rey();
+
             break;
 
             case 6:
@@ -100,6 +116,8 @@ void inicializar(int tablero[8][8]){
 }
 
 void imprimir(int tablero[8][8], int selec){
+
+    printf("\nLos movimientos validos para la pieza son: \n\n");
 
     for(int i = 0; i < 8; i++){
 
@@ -161,14 +179,28 @@ void imprimir(int tablero[8][8], int selec){
 
 void caballo(){
 
-    int tab[8][8];
+    int tab[8][8], cond = 1;
 
-    printf("\nDigite las cordenadas de la pieza:");
-    printf("\nPara x: ");
-    scanf("%d", &C1.x);
-    printf("\nPara y: ");
-    scanf("%d", &C1.y);
+    while (cond == 1){
 
+        printf("\nDigite las cordenadas de la pieza entre 1 y 8:");
+        printf("\nPara x: ");
+        scanf("%d", &C1.x);
+        printf("\nPara y: ");
+        scanf("%d", &C1.y);
+
+        if(C1.x > 0 && C1.x <= 8 && C1.y > 0 && C1.y <= 8){
+
+            cond = 2;
+
+        }else{
+
+            printf("\nPorfavor digite un valor entre 1 y 8 para ambas coordenadas.\n");
+
+        }
+    }
+
+    
     inicializar(tab);
 
     if(C1.x-1 > 1 && C1.y-1 > 0){
@@ -177,8 +209,336 @@ void caballo(){
         
     }
 
+    if(C1.x-1 > 0 && C1.y-1 > 1){
+
+        tab[C1.y-3][C1.x-2] = 2;
+        
+    }
+
+    if(C1.x-1 < 7 && C1.y-1 > 1){
+
+        tab[C1.y-3][C1.x] = 2;
+        
+    }
+
+    if(C1.x-1 < 6 && C1.y-1 > 0){
+
+        tab[C1.y-2][C1.x+1] = 2;
+        
+    }
+
+    if(C1.x-1 > 1 && C1.y-1 < 7){
+
+        tab[C1.y][C1.x-3] = 2;
+        
+    }
+
+    if(C1.x-1 > 0 && C1.y-1 < 6){
+
+        tab[C1.y+1][C1.x-2] = 2;
+        
+    }
+
+    if(C1.x-1 < 7 && C1.y-1 < 6){
+
+        tab[C1.y+1][C1.x] = 2;
+        
+    }
+
+    if(C1.x-1 < 6 && C1.y-1 < 7){
+
+        tab[C1.y][C1.x+1] = 2;
+        
+    }
+
     tab[C1.y-1][C1.x-1] = 1;
 
     imprimir(tab, 1);
+
+}
+
+void alfil(){
+
+    int tab[8][8], cond = 1;
+
+    while (cond == 1){
+
+        printf("\nDigite las cordenadas de la pieza entre 1 y 8:");
+        printf("\nPara x: ");
+        scanf("%d", &A1.x);
+        printf("\nPara y: ");
+        scanf("%d", &A1.y);
+
+        if(A1.x > 0 && A1.x <= 8 && A1.y > 0 && A1.y <= 8){
+
+            cond = 2;
+
+        }else{
+
+            printf("\nPorfavor digite un valor entre 1 y 8 para ambas coordenadas.\n");
+
+        }
+    }
+
+    inicializar(tab);
+
+    int contx = 0;
+    int conty = 0;
+
+    for(int i = 0; i < 8; i++){
+
+        for(int j = 0; j < 8; j++){
+
+            if(i == A1.x-1){
+                
+                if(i+contx >= 0 && i+contx<8 && j+A1.y-1 < 8 && j+A1.y-1 >= 0){
+
+                    tab[j+A1.y-1][i+contx] = 2;
+                    
+                }
+
+                if(j+A1.y-1 < 8 && j+A1.y-1 >= 0 && i-contx >= 0 && i-contx<8){
+
+                    tab[j+A1.y-1][i-contx]= 2;
+                    
+                }
+
+                if(i+contx >= 0 && i+contx<8 && j+A1.y-1-conty >= 0 && j+A1.y-1-conty < 8){
+
+                    tab[j+A1.y-1-conty][i+contx] = 2;
+
+                }
+
+                if(j+A1.y-1-conty >= 0 && j+A1.y-1-conty < 8 && i-contx >= 0 && i-contx<8){
+
+                    tab[j+A1.y-1-conty][i-contx]= 2;
+
+                }
+
+
+                contx++;
+                conty += 2;
+
+            }
+
+        }
+
+    }
+
+    tab[A1.y-1][A1.x-1] = 1;
+
+    imprimir(tab, 2);
+
+}
+
+void torre(){
+
+    int tab[8][8], cond = 1;
+
+    while (cond == 1){
+
+        printf("\nDigite las cordenadas de la pieza entre 1 y 8:");
+        printf("\nPara x: ");
+        scanf("%d", &T1.x);
+        printf("\nPara y: ");
+        scanf("%d", &T1.y);
+
+        if(T1.x > 0 && T1.x <= 8 && T1.y > 0 && T1.y <= 8){
+
+            cond = 2;
+
+        }else{
+
+            printf("\nPorfavor digite un valor entre 1 y 8 para ambas coordenadas.\n");
+
+        }
+    }
+
+    inicializar(tab);
+
+    for(int i = 0; i < 8; i++){
+
+        for(int j = 0; j < 8; j++){
+
+            if(i == T1.x-1){
+
+                tab[j][i] = 2;
+
+            }
+
+            if(j == T1.y-1){
+
+                tab[j][i] = 2;
+
+            }
+
+        }
+
+    }
+
+    tab[T1.y-1][T1.x-1] = 1;
+
+    imprimir(tab, 3);
+
+}
+
+void reina(){
+
+    int tab[8][8], cond = 1;
+
+    while (cond == 1){
+
+        printf("\nDigite las cordenadas de la pieza entre 1 y 8:");
+        printf("\nPara x: ");
+        scanf("%d", &Q1.x);
+        printf("\nPara y: ");
+        scanf("%d", &Q1.y);
+
+        if(Q1.x > 0 && Q1.x <= 8 && Q1.y > 0 && Q1.y <= 8){
+
+            cond = 2;
+
+        }else{
+
+            printf("\nPorfavor digite un valor entre 1 y 8 para ambas coordenadas.\n");
+
+        }
+    }
+
+    inicializar(tab);
+
+    int contx = 0;
+    int conty = 0;
+
+    for(int i = 0; i < 8; i++){
+
+        for(int j = 0; j < 8; j++){
+
+            if(i == Q1.x-1){
+                
+                if(i+contx >= 0 && i+contx<8 && j+Q1.y-1 < 8 && j+Q1.y-1 >= 0){
+
+                    tab[j+Q1.y-1][i+contx] = 2;
+                    
+                }
+
+                if(j+Q1.y-1 < 8 && j+Q1.y-1 >= 0 && i-contx >= 0 && i-contx<8){
+
+                    tab[j+Q1.y-1][i-contx]= 2;
+                    
+                }
+
+                if(i+contx >= 0 && i+contx<8 && j+Q1.y-1-conty >= 0 && j+Q1.y-1-conty < 8){
+
+                    tab[j+Q1.y-1-conty][i+contx] = 2;
+
+                }
+
+                if(j+Q1.y-1-conty >= 0 && j+Q1.y-1-conty < 8 && i-contx >= 0 && i-contx<8){
+
+                    tab[j+Q1.y-1-conty][i-contx]= 2;
+
+                }
+
+
+                contx++;
+                conty += 2;
+
+                tab[j][i] = 2;
+
+            }
+
+            if(j == Q1.y-1){
+
+                tab[j][i] = 2;
+
+            }
+
+        }
+
+    }
+
+    tab[Q1.y-1][Q1.x-1] = 1;
+
+    imprimir(tab, 4);
+
+}
+
+void rey(){
+
+    int tab[8][8], cond = 1;
+
+    while (cond == 1){
+
+        printf("\nDigite las cordenadas de la pieza entre 1 y 8:");
+        printf("\nPara x: ");
+        scanf("%d", &R1.x);
+        printf("\nPara y: ");
+        scanf("%d", &R1.y);
+
+        if(R1.x > 0 && R1.x <= 8 && R1.y > 0 && R1.y <= 8){
+
+            cond = 2;
+
+        }else{
+
+            printf("\nPorfavor digite un valor entre 1 y 8 para ambas coordenadas.\n");
+
+        }
+    }
+
+    inicializar(tab);
+
+    if(R1.x-1 > 0){
+
+        tab[R1.y-1][R1.x-2] = 2;
+
+    }
+
+    if(R1.x-1 < 7){
+
+        tab[R1.y-1][R1.x] = 2;
+
+    }
+
+    if(R1.y-1 > 0){
+
+        tab[R1.y-2][R1.x-1] = 2;
+
+    }
+
+    if(R1.y-1 < 7){
+
+        tab[R1.y][R1.x-1] = 2;
+
+    }
+
+    if(R1.x-1 > 0 && R1.y-1 > 0){
+
+        tab[R1.y-2][R1.x-2] = 2;
+
+    }
+
+    if(R1.x-1 < 7 && R1.y-1 > 0){
+
+        tab[R1.y-2][R1.x] = 2;
+
+    }
+
+    if(R1.x-1 > 0 && R1.y-1 < 7){
+
+        tab[R1.y][R1.x-2] = 2;
+
+    }
+
+     if(R1.x-1 < 7 && R1.y-1 < 7){
+
+        tab[R1.y][R1.x] = 2;
+
+    }
+
+    tab[R1.y-1][R1.x-1] = 1;
+
+    imprimir(tab, 5);
 
 }
